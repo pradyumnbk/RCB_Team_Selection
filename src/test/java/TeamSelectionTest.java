9b1157aae26db4357493f0a5e1c3e35d4f003935
pbarik <barik.pradyumn0@gmail.com>
2022-08-15 18:35:03 +0530

    Added description tag to the test script for allure reporting

--- src/test/java/TeamSelectionTest.java
+++ src/test/java/TeamSelectionTest.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.qameta.allure.Step;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,13 +17,14 @@ import java.util.List;
 
 public class TeamSelectionTest {
 
-    @Test
+    @Test(description = "Test to check rcb has 4 foreign player and one wicket keeper")
     public void teamSelection() throws IOException {
         try {
             ObjectMapper mapper = new ObjectMapper();
             PlayerList playerList=mapper.readValue(new File("./src/test/resources/team.json"),PlayerList.class);
             ArrayList<Player> players = playerList.getPlayer();
             //to check the foreign player count
+
             Assert.assertTrue(Team.countForeignPlayers(players),"The foreign player count is not equal to 4");
             //to check the wicket keeper count
             Assert.assertTrue(Team.countWicketPlayer(players),"There is no wicket keeper in the team ");
